7316283861194ef85a13541c21e357d095ea3f9b
Peter Colella <petercolella@Peters-MacBook-Pro.local>
2017-02-15 02:05:46 -0500

    unit 10

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,10 +1,28 @@
 console.log("JS file is connected to HTML! Woo!")
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-if (cardTwo === cardFour) {
-	window.alert("You found a match!")
-} else if (cardTwo !== cardFour) {
-	window.alert("Sorry, try again.")
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
+<!--
+//if (cardTwo === cardFour) {
+//	window.alert("You found a match!")
+//} else if (cardTwo !== cardFour) {
+//	window.alert("Sorry, try again.")
+//}
+-->
+var cards = ('queen', 'queen', 'king', 'king')
+
+var board = document.getElementById('game-board');
+
+function createBoard() {
+
+	for (var i=0; i<cards.length; i++) {
+
+		var cardElement = document.createElement('div');
+
+		cardElement.className = 'card';
+
+		board.appendChild(cardElement);
+	}
 }
+

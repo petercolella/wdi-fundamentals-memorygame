71fa782abe8b24960aac9c14b3b49c010f7103e7
Peter Colella <petercolella@Peters-MacBook-Pro.local>
2017-02-18 18:37:31 -0500

    Unit 11

--- starter-code/main.js
+++ starter-code/main.js
@@ -10,19 +10,67 @@ console.log("JS file is connected to HTML! Woo!")
 //	window.alert("Sorry, try again.")
 //}
 -->
-var cards = ('queen', 'queen', 'king', 'king')
+var cards = ['queen', 'queen', 'king', 'king'];
+
+var cardsInPlay = [];
 
 var board = document.getElementById('game-board');
 
 function createBoard() {
 
-	for (var i=0; i<cards.length; i++) {
+	for (var i = 0; i < cards.length; i++) {
 
 		var cardElement = document.createElement('div');
 
 		cardElement.className = 'card';
 
+		cardElement.setAttribute('data-card', cards[i]);
+
+		cardElement.addEventListener('click', isTwoCards);
+
+		board.appendChild(cardElement);
 		board.appendChild(cardElement);
 	}
+
 }
+
+//checks to see if there are cards in play
+function isTwoCards() {
+
+  // add card to array of cards in play
+  // 'this' hasn't been covered in this pre-work, but
+  // for now, just know it gives you access to the card the user clicked on
+  cardsInPlay.push(this.getAttribute('data-card'));
+
+  	console.log(this.getAttribute('data-card'));
+	if (this.getAttribute('data-card') === 'king') {
+		this.innerHTML = "<img src='king.png' alt='King of Spades'>"; // king
+	} else {
+		this.innerHTML = "<img src='queen.png' alt='Queen of Spades'>"; //queen
+	}
+
+  // if you have two cards in play, check for a match
+  if (cardsInPlay.length === 2) {
+
+    // pass the cardsInPlay as an argument to the isMatch function
+    isMatch(cardsInPlay);
+
+    // clear cards in play array for your next try
+    cardsInPlay = [];
+
+  }
+
+ function isMatch(cardsInPlay) {
+
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+	}
+
+}
+
+}
+
+createBoard();
 
